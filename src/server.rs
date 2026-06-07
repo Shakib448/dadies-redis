868a4336000c73bfdc5af369fe1391af9f71ec3f
Shakib448 <shakiba448@gmail.com>
2026-06-07 09:46:12 +0600

    WIP on redis server

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
 
+use anyhow::Ok;
 use tokio::net::TcpStream;
 use tokio::sync::{broadcast, mpsc, Semaphore}
+use tracing::{info, instrument};
 
 use crate::{Connection, Db, DbDropGuard, Shutdown};
 
@@ -56,4 +58,79 @@ pub async fn run(listener: TcpListener, shutdown: impl Future) {
     drop(shutdown_complete_tx);
 
     let _ = shutdown_complete_tx.recv().await;
+}
+
+impl Listener {
+    async fn run(&mut self) -> crate::Result<()> {
+        info!("accepting inbound connections");
+
+        loop {
+            let permit = self.limit_connections.clone().acquire_owned().await.unwrap();
+
+            let socket = self.accept().await?;
+
+            let mut handler = Handler {
+                db: self.db_holder.db(),
+                connection : Connection::new(socket),
+                shutdown : Shutdown::new(self.notify_shutdown.subscribe()),
+                _shutdown_complete: self.shutdown_complete_tx.clone(),
+            };
+
+            tokio::spwan(async move {
+                if let Err(err) = handler.run().await {
+                    error!(cause = %err, "Error running handler");
+                }
+                drop(permit);
+            });
+
+        }
+    }
+
+    async fn accept(&mut self) -> crate::Result<TcpStream> {
+        let mut backoff = 1;
+
+        loop {
+            match self.listener.accept().await {
+                Ok((socket, _)) => return Ok(socket),
+                Err(err) => {
+                    if backoff > 64 {
+                        return Err(err.into())
+                    }
+                }
+            }
+            time::sleep(Duration::from_millis(backoff)).await;
+            backoff *= 2;
+        }
+    }
+}
+
+impl Handler {
+
+    #[instrument(skip(self))]
+    async fn run(&mut self) -> crate::Result<() > {
+        while !self.shutdown.is_shutdown() {
+            let maybe_frame = tokio::select! {
+                res = self.connection.read_frame() => res?,
+                _ = self.shutdown.recv() => {
+                    return Ok(());
+                }
+            };
+        let frame = match maybe_frame {
+            Some(frame) => frame,
+            None => return Ok(()),
+        };
+
+        let cmd = Command::from_frame(frame)?;
+
+
+        debug!(?cmd);
+
+
+        cmd.apply(&self.db, &mut self.connection, &mut self.shutdown).await?;
+        };
+
+        Ok(())
+
+    }
+
 }
